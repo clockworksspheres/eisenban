259c72c0af6337e9e77d35fd6dd5042a565994db
Roy Nielsen <roy.nielsen17@gmail.com>
2021-08-07 13:40:47 -0400

    filled out getters, setters, __repr__ and __string__ for the priority class

--- eisenban/model/priority.py
+++ eisenban/model/priority.py
@@ -2,19 +2,33 @@
 class priority(object):
 
   """
-  :version:
-  :author:
-  """
+  :version: 0.1
+  :author: Roy Nielsen
 
-  """ ATTRIBUTES
+  ATTRIBUTES
 
-  niceLvl  (private)
+  The priority data structure has the following layout: 
 
-  urgent  (private)
+  niceLevel: Similar to the *nix "nice command" where the lower (even negative) priority, the more important this is.
 
-  important  (private)
+  urgent:  True or False, per the Eisenhower Productivity Matrix
 
-  priorityNotes  (private)
+  important:  True or False, per the Eisenhower Productivity Matrix
+
+  priorityNotes: Any kind of note that needs to be recorded due to a priority assignment or change.
+
+  References to definitions and usage of a Eisenhower Productivity Matrix:
+    * [Eisenhower Matrix: How to Prioritise and Master Productivity](https://www.techtello.com/eisenhower-productivity-matrix/)
+    * [The Eisenhower Matrix](https://todoist.com/productivity-methods/eisenhower-matrix)
+    * [How to overcome procrastination with the Eisenhower matrix](https://www.lucidchart.com/blog/eisenhower-matrix)
+    * [Implementing the Eisenhower Matrix in Evernote](https://filterize.net/blog/using-filterize/eisenhower-matrix/)
+
+  References to other Eisenhower Productivity Matrix projects on Github:
+    * [https://github.com/LakithKarunaratne/Eisenhower](https://github.com/LakithKarunaratne/Eisenhower)
+    * [https://github.com/Lleafll/eisenhower](https://github.com/Lleafll/eisenhower)
+    * [https://github.com/Ghalko/ironChart](https://github.com/Ghalko/ironChart)
+    * [https://github.com/maciek-nowak/eisenhower-matrix](https://github.com/maciek-nowak/eisenhower-matrix)
+    * [https://github.com/sthompson232/Eisenhowers-Quadrant](https://github.com/sthompson232/Eisenhowers-Quadrant)
 
   """
 
@@ -27,9 +41,27 @@ class priority(object):
     @param bool important : 
     @param string priorityNotes : 
     @return  :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    if isinstance(niceLevel, int) and niceLevel < 20 and niceLevel > -20:
+      self.niceLevel = niceLevel
+    else:
+      self.niceLevel = 0
+
+    if isinstance(urgent, bool):
+      self.urgent = urgent
+    else:
+      self.urgent = True
+
+    if isinstance(important, bool):
+      self.important = important
+    else:
+      self.important = True
+
+    if isinstance(priorityNotes, str):
+      self.priorityNotes = priorityNotes
+    else:
+      priorityNotes = ""
 
   def setNiceLevel(self, newNiceLevel = 0):
     """
@@ -39,9 +71,12 @@ class priority(object):
 
     @param short int newNiceLevel : 
     @return  :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    if isinstance(newNiceLevel, int) and newNiceLevel < 20 and newNiceLevel > -20:
+      self.niceLevel = newNiceLevel
+    else:
+      self.niceLevel = 0
 
   def setImportant(self, important = True):
     """
@@ -49,9 +84,12 @@ class priority(object):
 
     @param bool important : 
     @return  :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    if isinstance(important, bool):
+      self.important = important
+    else:
+      self.important = True
 
   def setUrgent(self, urgent = True):
     """
@@ -59,9 +97,12 @@ class priority(object):
 
     @param bool urgent : 
     @return  :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    if isinstance(urgent, bool):
+      self.urgent = urgent
+    else:
+      self.urgent = True
 
   def setPrioityNotes(self, priorityNotes = ""):
     """
@@ -70,9 +111,12 @@ class priority(object):
 
     @param string priorityNotes : 
     @return  :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    if isinstance(priorityNotes, str):
+      self.priorityNotes = priorityNotes
+    else:
+      priorityNotes = ""
 
   def getNiceLevel(self, niceLevel = 0):
     """
@@ -80,9 +124,9 @@ class priority(object):
 
     @param signed int niceLevel : 
     @return string :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    return self.niceLevel
 
   def getImportant(self, important = True):
     """
@@ -90,9 +134,9 @@ class priority(object):
 
     @param bool important : 
     @return bool :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    return self.important
 
   def getUrgent(self, urgent = True):
     """
@@ -100,9 +144,9 @@ class priority(object):
 
     @param bool urgent : 
     @return bool :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    return self.urgent
 
   def getPriorityNotes(self, priorityNotes = ""):
     """
@@ -110,9 +154,23 @@ class priority(object):
 
     @param string priorityNotes : 
     @return string :
-    @author
+    @author : Roy Nielsen
     """
-    pass
+    return self.priorityNotes
 
+    def __str__(self):
+      """
+      :return: Return a string representation of the class data
+      """
+      return "niceLvl: {}, urgent: {}, important: {}, priorityNotes: {}".format(self.niceLevel, self.urgent, self.important, self.priorityNotes)
 
-
+    def __repr__(self):
+      """
+      :return: Return the data structure represented by this class
+      """
+      return {priority:
+                { "niceLvl": self.niceLevel,
+                  "urgent": self.urgent,
+                  "important": self.important,
+                  "priorityNotes": self.priorityNotes }
+             }
