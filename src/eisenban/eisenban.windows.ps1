92cbb11cbb6345098a8acce7a068e2832a11afc9
Roy <11579416+roynielsen17@users.noreply.github.com>
2025-02-27 09:56:57 -0700

    adding windows shell file

---
+++ src/eisenban/eisenban.windows.ps1
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# highly modified version of:
+# https://www.pythonguis.com/tutorials/packaging-pyside6-applications-pyinstaller-macos-dmg/
+# amoung others... including
+# https://pyinstaller.org/en/stable/
+
+#if doesn't exist...
+# cd to the eisenban source root
+ 
+# python3 -m venv packenv
+# packenv/Scripts/Activate.ps1
+
+# pip3 install PySide6 PyInstaller
+# pip3 install --upgrade PyInstaller pyinstaller-hooks-contrib
+
+#pyinstaller --clean -y eisenban.windows.onefile.spec
+#pyinstaller -y eisenban.windows.onefile.spec
+
+
+
