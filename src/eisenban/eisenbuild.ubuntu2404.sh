f05f21ac575bba0760d7935717bcf0ea517c25fc
Roy Nielsen <roy.nielsen17@gmail.com>
2025-03-05 00:08:47 -0700

    fixing ubuntu build script

--- src/eisenban/eisenbuild.ubuntu2404.sh
+++ src/eisenban/eisenbuild.ubuntu2404.sh
@@ -9,7 +9,7 @@
 
 directory="./packenv"
 actfile="./packenv/bin/activate"
-if [ ! -d "$directory" ]  || [ ! -f "$actfile"]; then
+if [ ! -d "$directory" ]  || [ ! -f "$actfile" ] ; then
    python3 -m venv packenv
    source packenv/bin/activate
 
@@ -20,7 +20,7 @@ else
 fi
 
 pyinstaller --clean -y eisenbuild.linux.onefile.spec
-pyinstaller -y eisenbuild.linux.onefile.spec
+pyinstaller -y eisenbuild.linux.py312.onefile.spec
 #cp -a resources dist/eisenban.app/Contents/MacOS
 #cp -a resources dist/eisenban.app/Contents/Resources
 
