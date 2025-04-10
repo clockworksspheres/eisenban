633aa389fd6da179583126295cd48d8f38776eeb
Roy <11579416+roynielsen17@users.noreply.github.com>
2025-04-10 14:41:34 -0600

    churn

--- src/eisenban/proto/process/ele-009.py
+++ src/eisenban/proto/process/ele-009.py
@@ -2,23 +2,31 @@
 import subprocess
 import getpass
 
-passwd = getpass.getpass('Password: ')
-# print('Password entered:', passwd)
 
 
-# Define the password and the command
-command = 'ps -ef'.split()
+def run_cmd_w_sudo(passwd):
+    # Define the password and the command
+    command = 'ps -ef'.split()
 
-sudocmd = ["sudo", "-s"] + command
+    sudocmd = ["sudo", "-s"] + command
 
-# Create the subprocess
-proc = subprocess.Popen(sudocmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Create the subprocess
+    proc = subprocess.Popen(sudocmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# Communicate the password to the subprocess
-output, error = proc.communicate((passwd + '\n').encode())
+    # Communicate the password to the subprocess
+    output, error = proc.communicate(passwd)
 
-# Print the output and error
-print("Output:", output)
-# print("Error:", error)
+    # Print the output and error
+    print("Output:", output)
+    # print("Error:", error)
+
+if __name__ == "__main__":
+    
+    passwd = getpass.getpass('Password: ')
+    # print('Password entered:', passwd)a
+    passwd = (passwd + "\n").encode()
+    run_cmd_w_sudo(passwd)
+
+
 
 
