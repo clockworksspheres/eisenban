b7a14413223ef6b9571ec9d70b63867120ad89fe
Roy Nielsen <roy.nielsen17@gmail.com>
2025-05-01 11:48:51 -0600

    churn

--- src/eisenban/lib/environment.py
+++ src/eisenban/lib/environment.py
@@ -14,13 +14,19 @@ import socket
 import subprocess
 # import types
 import platform
-import pwd
 import time
 import traceback
 
 sys.path.append("../..")
 
-from eisenban.config import DEFAULT_LOG_LEVEL, LogPriority 
+from eisenban.config import DEFAULT_LOG_LEVEL, LogPriority
+
+if sys.platform.startswith('win32'):
+    import win32api
+    from eisenban.lib.windows_utilities import is_windows_process_elevated
+
+else:
+    import pwd
 
 try:
     from eisenban.lib.localize import VERSION
@@ -35,7 +41,16 @@ try:
 except ImportError or AssertionError:
     FISMACAT = 'low'
 
-if os.geteuid() == 0:
+euid = 90000000
+process_is_elevated = False
+if sys.platform.startswith("win32"):
+    if is_windows_process_elevated():
+        process_is_elevated = True
+else:
+    euid = os.geteuid()
+    if os.geteuid() == 0:
+        process_is_elevated = True
+if process_is_elevated:
     try:
         import dmidecode
         DMI = True
@@ -47,6 +62,7 @@ else:
 # third party libraries
 from eisenban.lib.run_commands import RunWith as RunWith
 
+
 class Environment(object):
     """
     The Environment class collects commonly used information about the
@@ -56,6 +72,7 @@ class Environment(object):
     """
 
     def __init__(self):
+        self.rw = RunWith()
         self.operatingsystem = ''
         self.osreportstring = ''
         self.osfamily = ''
@@ -69,8 +86,11 @@ class Environment(object):
         self.systemtype = ''
         self.numrules = 0
         self.version = VERSION
-        self.euid = os.geteuid()
-        currpwd = pwd.getpwuid(self.euid)
+        if sys.platform.startswith("win32"):
+            self.euid = win32api.GetUserName()
+        else:
+             self.euid = os.geteuid()
+             currpwd = pwd.getpwuid(self.euid)
         self.test_mode = ""
         self.script_path = ""
         self.resources_path = ""
@@ -89,7 +109,6 @@ class Environment(object):
         self.systemfismacat = 'low'
         self.determinefismacat()
         self.collectinfo()
-        self.rw = RunWith()
 
     def setsystemtype(self):
         '''
@@ -107,6 +126,7 @@ class Environment(object):
         cmdlocs = ["/usr/bin/ps", "/bin/ps"]
         cmdbase = ""
         cmd = ""
+        self.systemtype = ""
 
         # buld the command
         for cl in cmdlocs:
@@ -119,13 +139,18 @@ class Environment(object):
 
             if cmd:
                 # run the command
-                cmdoutput = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, close_fds=True, text=True)
-                outputlines = cmdoutput.stdout.readlines()
+                self.rw.setCommand(cmd)
+                output, _, _ = self.rw.communicate()
+                #cmdoutput = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, close_fds=True, text=True)
+                #outputlines = cmdoutput.stdout.readlines()
+                outputlines = output.split("\n")
                 for line in outputlines:
                     line = str(line)
+                    # print("        line: " + str(line))
                     for vt in validtypes:
                         if re.search(vt, line, re.IGNORECASE):
                             self.systemtype = vt
+                            # print("type: " + str(vt))
 
             else:
                 print(str(__name__) + ":Unable to determine systemtype. Required utility 'ps' does not exist on this system")
@@ -345,11 +370,15 @@ class Environment(object):
         """
         # Alternative (better) implementation for Linux
         if os.path.exists('/usr/bin/lsb_release'):
-            proc = subprocess.Popen('/usr/bin/lsb_release -dr',
-                                    shell=True, stdout=subprocess.PIPE,
-                                    close_fds=True, text=True)
-            description = proc.stdout.readline()
-            release = proc.stdout.readline()
+            self.rw.setCommand(["/usr/bin/lsb_release", "-dr"])
+            output, _, _ = self.rw.communicate()
+            #proc = subprocess.Popen('/usr/bin/lsb_release -dr',
+            #                        shell=True, stdout=subprocess.PIPE,
+            #                        close_fds=True, text=True)
+            #description = proc.stdout.readline()
+            output = output.splitlines()
+            description = output[0]
+            release = output[1]
             description = description.split()
             # print description
             del description[0]
@@ -417,24 +446,22 @@ class Environment(object):
                 self.osreportstring = self.operatingsystem +  ' ' + self.osversion
 
         elif os.path.exists('/usr/bin/sw_vers'):
-            proc1 = subprocess.Popen('/usr/bin/sw_vers -productName',
-                                     shell=True, stdout=subprocess.PIPE,
-                                     close_fds=True, text=True)
-            description = proc1.stdout.readline()
+            self.rw.setCommand(["/usr/bin/sw_vers", "-productName"])
+            output, _, _ = self.rw.communicate()
+            print("Product Name: " + str(output))
+            description = output
             description = description.strip()
-            proc2 = subprocess.Popen('/usr/bin/sw_vers -productVersion',
-                                     shell=True, stdout=subprocess.PIPE,
-                                     close_fds=True, text=True)
-            release = proc2.stdout.readline()
-            release = release.strip()
+
+            self.rw.setCommand(["/usr/bin/sw_vers", "-productVersion"])
+            output, _, _ = self.rw.communicate()
+            release = output.strip()
             self.operatingsystem = description
             self.osversion = release
 
-            proc3 = subprocess.Popen('/usr/bin/sw_vers -buildVersion',
-                                     shell=True, stdout=subprocess.PIPE,
-                                     close_fds=True, text=True)
-            build = proc3.stdout.readline()
-            build = build.strip()
+            self.rw.setCommand("/usr/bin/sw_vers", "-buildVersion")
+            output, _, _ = self.rw.communicate()
+            build = output.strip()
+
             opsys = str(description) + ' ' + str(release) + ' ' + str(build)
             self.osreportstring = opsys
 
@@ -510,9 +537,9 @@ class Environment(object):
         fuzzy classification of the OS.
         """
         uname = sys.platform
-        if uname.startswith("linux"):
+        if uname.lower().startswith("linux"):
             self.osfamily = 'linux'
-        elif uname == 'darwin':
+        elif uname.lower() == 'darwin':
             self.osfamily = 'darwin'
         elif uname == 'sunos5':
             self.osfamily = 'solaris'
@@ -525,6 +552,11 @@ class Environment(object):
         make an educated guess as to the correct network data. self.ipaddress
         and self.macaddress will be updated by this method.
         """
+        if sys.platform.startswith('darwin'):
+            self.hostname = ''
+            self.ipaddress = ''
+            self.macaddress = ''
+            return
         # regex to match mac addresses
         macre = '(([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})'
 
@@ -533,9 +565,9 @@ class Environment(object):
 
         hostname = socket.getfqdn()
         try:
-            ipdata = socket.gethostbyname_ex(hostname)
-            iplist = ipdata[2]
             try:
+                ipdata = socket.gethostbyname_ex(hostname)
+                iplist = ipdata[2]
                 iplist.remove('127.0.0.1')
             except ValueError:
                 # tried to remove loopback when it's not present, continue
@@ -551,15 +583,18 @@ class Environment(object):
 
         # In ifconfig output macaddresses are always one line before the ip
         # address.
-        if sys.platform == 'linux2':
+        if sys.platform.startswith("linux"):
             cmd = '/sbin/ifconfig'
         elif os.path.exists('/usr/sbin/ifconfig'):
             cmd = '/usr/sbin/ifconfig -a'
         else:
             cmd = '/sbin/ifconfig -a'
-        proc = subprocess.Popen(cmd, shell=True,
-                                stdout=subprocess.PIPE, close_fds=True, text=True)
-        netdata = proc.stdout.readlines()
+        self.rw.setCommand(cmd)
+        output, _, _ = self.rw.communicate()
+        netdata = output
+        #proc = subprocess.Popen(cmd, shell=True,
+        #                        stdout=subprocess.PIPE, close_fds=True, text=True)
+        #netdata = proc.stdout.readlines()
 
         for line in netdata:
             line = str(line)
@@ -586,6 +621,10 @@ class Environment(object):
         @change: 2017/9/20 - bgonz12 - Changed implementation to not branch
                     conditionally by OS, but to branch by file system searches.
         """
+        if sys.platform.startswith('darwin'):
+            ipaddr = ''
+            gateway = ''
+            return ipaddr
         ipaddr = '127.0.0.1'
         gateway = ''
         if os.path.exists('/usr/bin/lsb_release'):
@@ -609,6 +648,10 @@ class Environment(object):
                     cmd = '/usr/sbin/route -n get default'
                 else:
                     cmd = '/sbin/route -n get default'
+                #self.rw.setCommand(cmd)
+                #output, _, _ = self.rw.communicate()
+                #print(str(output))
+                #routdata = output.strip()
                 routecmd = subprocess.Popen(cmd, shell=True,
                                             stdout=subprocess.PIPE,
                                             close_fds=True, text=True)
@@ -630,6 +673,9 @@ class Environment(object):
                 if len(matched) == 1:
                     ipaddr = matched[0]
                     break
+        if sys.platform.startswith('darwin'):
+            ipaddr = ''
+            gateway = ''
         return ipaddr
 
     def matchip(self, target, iplist, level=1):
@@ -682,10 +728,13 @@ class Environment(object):
         elif os.path.exists('/sbin/ifconfig'):
             cmd = '/sbin/ifconfig -a'
         try:
+            #self.rw.setCommand(cmd)
             ifcmd = subprocess.Popen(cmd, shell=True,
                                      stdout=subprocess.PIPE,
                                      close_fds=True, text=True)
             ifdata = ifcmd.stdout.readlines()
+            #output, _, _ = self.rw.communicate()
+            #ifdata = output.strip()
         except(OSError):
             # self.logdispatch, self.logger are not used in this file, as this code is intended to be run before
             # a logger is loaded
@@ -736,11 +785,15 @@ class Environment(object):
                 pass
         elif os.path.exists('/usr/sbin/system_profiler'):
             profilerfetch = '/usr/sbin/system_profiler SPHardwareDataType'
+            self.rw.setCommand(profilerfetch)
+            output, _, _ = self.rw.communicate()
+            '''
             cmd3 = subprocess.Popen(profilerfetch, shell=True,
                                     stdout=subprocess.PIPE,
                                     close_fds=True, text=True)
             cmd3output = cmd3.stdout.readlines()
-            for line in cmd3output:
+            '''
+            for line in output.splitlines():
                 line = line.strip()
                 if re.search('Serial Number (system):', line):
                     line = line.split(':')
@@ -835,10 +888,12 @@ class Environment(object):
                 pass
         elif os.path.exists('/usr/sbin/dmidecode') and self.euid == 0:
             uuidfetch = '/usr/sbin/dmidecode -s system-uuid'
-            cmd1 = subprocess.Popen(uuidfetch, shell=True,
-                                    stdout=subprocess.PIPE,
-                                    close_fds=True, text=True)
-            uuid = cmd1.stdout.readline()
+            self.rw.setCommand(uuidfetch)
+            uuid, _, _ = self.rw.communicate()
+            #cmd1 = subprocess.Popen(uuidfetch, shell=True,
+            #                        stdout=subprocess.PIPE,
+            #                        close_fds=True, text=True)
+            #uuid = cmd1.stdout.readline()
         elif os.path.exists('/usr/sbin/smbios'):
             smbiosfetch = '/usr/sbin/smbios -t SMB_TYPE_SYSTEM 2>/dev/null'
             cmd2 = subprocess.Popen(smbiosfetch, shell=True,
@@ -855,11 +910,15 @@ class Environment(object):
                         pass
         elif os.path.exists('/usr/sbin/system_profiler'):
             profilerfetch = '/usr/sbin/system_profiler SPHardwareDataType'
+            self.rw.setCommand(profilerfetch)
+            output, _, _ = self.rw.communicate()
+            '''
             cmd3 = subprocess.Popen(profilerfetch, shell=True,
                                     stdout=subprocess.PIPE,
                                     close_fds=True, text=True)
             cmd3output = cmd3.stdout.readlines()
-            for line in cmd3output:
+            '''
+            for line in output.splitlines():
                 line = line.strip()
                 if re.search('UUID:', line):
                     line = line.split()
