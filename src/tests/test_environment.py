8509d3a1e7d2cfa4b55cf90602c4ab055c57c02c
Roy <roy.nielsen17@gmail.com>
2025-07-17 11:28:56 -0400

    added Rock to valid OS's

--- src/tests/test_environment.py
+++ src/tests/test_environment.py
@@ -43,7 +43,7 @@ class test_environment(unittest.TestCase):
 
     def testGetostype(self):
         tracemalloc.start(10)
-        validtypes = 'Red Hat Enterprise Linux|Debian|Ubuntu|CentOS|Fedora|' + \
+        validtypes = 'Red Hat Enterprise Linux|AlmaLinux|Rocky Linux|Debian|Ubuntu|CentOS|Fedora|' + \
                      'openSUSE|Mac OS X|macOS'
         print('OS Type: ' + str(self.to.getostype()))
         self.assertTrue(re.search(validtypes, self.to.getostype()))
