0f1b69e7644a1f62c038b8115bddf6cd6c8245f2
Roy <roynielsen17@gmail.com>
2025-11-21 07:10:23 -0700

    updating macos build script

--- src/eisenban/eisenbuild.macos.sh
+++ src/eisenban/eisenbuild.macos.sh
@@ -16,7 +16,7 @@ if [ ! -d "$directory" ]  || [ ! -f "$actfile" ] ; then
    python -m venv packenv
    source packenv/bin/activate
    pip install --upgrade pip
-   pip install Tk
+   pip install --force-reinstall --no-cache-dir Tk
    pip install PySide6 PyInstaller
    pip install --upgrade PyInstaller pyinstaller-hooks-contrib
 else
@@ -31,8 +31,12 @@ export PATH=".":$PATH
 
 pushd ui; python3 compile_uifiles.py; popd
 
+echo "----- start pyinstaller --clean -y eisenbuild.macos.spec -----"
 pyinstaller --clean -y eisenbuild.macos.spec
+echo "----- end pyinstaller --clean -y eisenbuild.macos.spec -----"
+echo " ===== start pyinstaller -y eisenbuild.macos.spec ====="
 pyinstaller -y eisenbuild.macos.spec
+echo "===== end pyinstaller -y eisenbuild.macos.spec ====="
 ### DOES NOT WORK... need to figure out why...
 cp -a resources dist/eisenban.app/Contents/Resources
 cp -a resources dist/eisenban.app/Contents
