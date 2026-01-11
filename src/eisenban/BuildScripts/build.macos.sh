a797204ea6d090e32a6785f5b2b9afd0106e79c1
Roy <roynielsen17@gmail.com>
2026-01-11 06:14:25 -0700

    build script mods

--- src/eisenban/BuildScripts/build.macos.sh
+++ src/eisenban/BuildScripts/build.macos.sh
@@ -33,15 +33,14 @@ export PATH=".":$PATH
 
 pushd ui; python3 compile_uifiles.py; popd
 
-
-cp BuildScripts/eisenbuild.macos.spec .
+cp BuildScripts/build.macos.spec .
 
 echo "----- start pyinstaller --clean -y eisenbuild.macos.spec -----"
-pyinstaller --clean -y eisenbuild.macos.spec
+pyinstaller --clean -y build.macos.spec
 echo "----- end pyinstaller --clean -y eisenbuild.macos.spec -----"
 echo " ===== start pyinstaller -y eisenbuild.macos.spec ====="
-pyinstaller -y eisenbuild.macos.spec
-rm eisenbuild.macos.spec
+pyinstaller -y build.macos.spec
+rm build.macos.spec
 echo "===== end pyinstaller -y eisenbuild.macos.spec ====="
 ### DOES NOT WORK... need to figure out why...
 cp -a resources dist/eisenban.app/Contents/Resources
