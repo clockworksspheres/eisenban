098a9c6c1c6b0edc60af3ed217b492b35eb08e17
Roy <roynielsen17@gmail.com>
2026-05-22 14:14:09 -0500

    working with newer python via pyenv for Callable testing

--- src/BuildScripts/build.macos.sh
+++ src/BuildScripts/build.macos.sh
@@ -18,7 +18,7 @@ pushd ..
 directory="./packenv"
 actfile="./packenv/bin/activate"
 if [ ! -d "$directory" ]  || [ ! -f "$actfile" ] ; then
-   python3 -m venv packenv
+   python -m venv packenv
    source packenv/bin/activate
 
    pip install --upgrade pip
